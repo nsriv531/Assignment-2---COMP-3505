c1124026cb0130caca0b23a4603860aab4747887
acauf564 <acauf564@mtroyal.ca>
2026-02-04 14:55:06 -0700

    Added method stubs for setup/teardown

--- test/org/jfree/data/CombineTest.java
+++ test/org/jfree/data/CombineTest.java
@@ -1,14 +1,32 @@
 package org.jfree.data;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CombineTest {
 
+	private Range exampleRange;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
-
+	@AfterEach
+  	void tearDown() throws Exception {
+  	}
+	
+	@AfterAll
+  	static void tearDownAfterClass() throws Exception {
+  	}
 }
--- test/org/jfree/data/ContainsTest.java
+++ test/org/jfree/data/ContainsTest.java
@@ -1,14 +1,33 @@
 package org.jfree.data;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ContainsTest {
 
+	private Range exampleRange;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
+	@AfterEach
+  	void tearDown() throws Exception {
+  	}
+	
+	@AfterAll
+  	static void tearDownAfterClass() throws Exception {
+  	}
 
 }
