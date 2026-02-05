41d009aa1f1cee968bb0e0700712ba47207ca27e
nsriv531 <nsriv531@mtroyal.ca>
2026-02-04 17:47:39 -0700

    Made temporary tests for EqualsTest.java

--- test/org/jfree/data/EqualsTest.java
+++ test/org/jfree/data/EqualsTest.java
@@ -6,9 +6,42 @@ import org.junit.jupiter.api.Test;
 
 class EqualsTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    void testSameObjectReturnsTrueForEquals() {
+        Range r = new Range(1.0, 5.0);
+        assertTrue(r.equals(r));
+    }
 
+    @Test
+    void testEquivalentRangeReturnsTrueForEquals() {
+        Range r1 = new Range(1.0, 5.0);
+        Range r2 = new Range(1.0, 5.0);
+        assertTrue(r1.equals(r2));
+    }
+
+    @Test
+    void testDifferentLowerReturnsFalseForEquals() {
+        Range r1 = new Range(1.0, 5.0);
+        Range r2 = new Range(2.0, 5.0);
+        assertFalse(r1.equals(r2));
+    }
+
+    @Test
+    void testDifferentUpperReturnsFalseForEquals() {
+        Range r1 = new Range(1.0, 5.0);
+        Range r2 = new Range(1.0, 6.0);
+        assertFalse(r1.equals(r2));
+    }
+
+    @Test
+    void testNullReturnsFalseForEquals() {
+        Range r = new Range(1.0, 5.0);
+        assertFalse(r.equals(null));
+    }
+
+    @Test
+    void testDifferentTypeReturnsFalseForEquals() {
+        Range r = new Range(1.0, 5.0);
+        assertFalse(r.equals("not a Range"));
+    }
 }
